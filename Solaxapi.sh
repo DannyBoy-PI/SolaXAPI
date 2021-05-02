9e90d3690baf28dabd27303198634374b00bf751
DannyBoy-PI <82942385+DannyBoy-PI@users.noreply.github.com>
2021-05-02 19:01:46 +0100

    Update Solaxapi.sh

--- Solaxapi.sh
+++ Solaxapi.sh
@@ -1,4 +1,10 @@
 #
 # Example BASH script for Raspberry Pi (Assumes you have the PHP script in /home/pi)
 #
+# If you want to create a subdirectory called Solax uncomment the line below and 
+# make sure you change the lines below to include the subdirectory and the cron job
+# entry.
+
+# cd Solax
+
 php /home/pi/Solax-pvoutput.php >> "/home/pi/SolaX-"$(date '+%Y-%m-%d')"-log.solaxapi"
